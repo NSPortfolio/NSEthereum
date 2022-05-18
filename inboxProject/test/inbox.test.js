302d8d452997f2f16bed66130ba1558ecf89fda2
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-17 20:45:52 -0500

    Update inbox.test.js

--- inboxProject/test/inbox.test.js
+++ inboxProject/test/inbox.test.js
@@ -3,4 +3,13 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+});
+
+describe('Inbox', () => {
+  it('deploy', () => {});
+});
