d98686f4d9e2760f2dcd8b9a9fa88fc03603ad0f
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-17 20:30:07 -0500

    Update inbox.test.js

--- inboxProject/test/inbox.test.js
+++ inboxProject/test/inbox.test.js
@@ -3,19 +3,4 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return 'stopped';
-  }
-  drive() {
-    return 'vroom';
-  }
-}
 
-describe('Car', () => {
-  it('park', () => {
-    const car = new Car();
-    assert.equal(car.park(), 'stopped');
-    assert.equal(car.drive(), 'vroom');
-  });
-});
