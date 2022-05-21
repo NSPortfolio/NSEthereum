e79b899b18ea80a93cbdb398c559fdc48b760505
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-21 13:35:05 -0500

    Create lottery.js

---
+++ LotteryWebApp/src/lottery.js
@@ -0,0 +1,5 @@
+import web3 from './web3';
+const address = '0xE0F99926840A3376bDC3Ad36f325892C5333E9Fe';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"input":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","output":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address);
