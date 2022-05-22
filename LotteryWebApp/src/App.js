21e9ed7e6e25a94fde97ea0edaddfd241ad25f88
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-22 18:18:01 -0500

    Update App.js

--- LotteryWebApp/src/App.js
+++ LotteryWebApp/src/App.js
@@ -2,24 +2,67 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  state = {
+    manager = '',
+    players: [],
+    balance: '';
+    value: '',
+    message: '',
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers.call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this setState({ manager, players, balance });
+  }
+  
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction...' });
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: wb3.utils.toWei(this.state.value, 'ether');
+    });
+    this.setState({ message:'You have been entered!' });
+  };
+
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message : 'Waiting on transaction...'});
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+    this.setState({ message:'A winner has been picked!' });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}.
+           There are currently {this.state.players.length} people entered competing to win {web.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4> Want to try your luck? </h4>
+          <div>
+            <label> Amount of ether to enter </label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setstate({ value:event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onClick}>Pick A Winner!</button>
+        <hr />
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
