c9de62b994bd52ba96bab192725a23bda5e9f3ff
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-28 13:37:19 -0500

    Update Layout.js

--- CrowdFundingWebApp/components/Layout.js
+++ CrowdFundingWebApp/components/Layout.js
@@ -5,6 +5,7 @@ import Header from './Header';
 export default () => {
   return (
     <Container>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
       <Header/>
       {props.children}
     </Container>
