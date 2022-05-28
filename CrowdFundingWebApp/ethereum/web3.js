6153d1e558e3895331b68b6af89b107d497a575a
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-28 10:44:14 -0500

    Update web3.js

--- CrowdFundingWebApp/ethereum/web3.js
+++ CrowdFundingWebApp/ethereum/web3.js
@@ -6,8 +6,9 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    
+    'https://rinkeby.infura.io/v3/ab31001bf2764fc880d447c45d2046b4'
   );
+  web3 = new Web3(provider);
 }
 
 export default web3;
