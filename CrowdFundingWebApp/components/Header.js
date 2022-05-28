28d1af374b9fd6ab4a8cb505e4e11c11e3d4091d
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-28 13:23:16 -0500

    Update Header.js

--- CrowdFundingWebApp/components/Header.js
+++ CrowdFundingWebApp/components/Header.js
@@ -3,11 +3,10 @@ import { Menu } from 'semantic-ui-react';
 
 export default () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: '10px' }}>
       <Menu.Item>
         CrowdCoin
       </Menu.Item>
-    
       <Menu.Menu position="right">
         <Menu.Item>
           Campaigns
