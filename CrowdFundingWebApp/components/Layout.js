41a52da501a9f228cfe19443658497a40a5931a8
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-28 13:39:30 -0500

    Update Layout.js

--- CrowdFundingWebApp/components/Layout.js
+++ CrowdFundingWebApp/components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import Header from './Header';
+import Head from 'next/head';
 
 export default () => {
   return (
     <Container>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"/>
+      </Head>
       <Header/>
       {props.children}
     </Container>
