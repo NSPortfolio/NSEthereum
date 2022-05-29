fd30967360bd2bc0adb2230e2526158918937834
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-29 08:56:37 -0500

    Update show.js

--- CrowdFundingWebApp/pages/campaigns/show.js
+++ CrowdFundingWebApp/pages/campaigns/show.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    props.query.address;
+  }
   render() {
     return (
       <Layout>
