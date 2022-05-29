f8aeafaca388ddb6f79385c148fc02abdefcfd2d
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-29 09:54:32 -0500

    Update show.js

--- CrowdFundingWebApp/pages/campaigns/show.js
+++ CrowdFundingWebApp/pages/campaigns/show.js
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import { Card } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
