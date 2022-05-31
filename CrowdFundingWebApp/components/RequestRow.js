50c50ed2f0092dc2cdf7b124c2ab6e1fc538fc06
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-30 21:08:17 -0500

    Update RequestRow.js

--- CrowdFundingWebApp/components/RequestRow.js
+++ CrowdFundingWebApp/components/RequestRow.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Table } from 'semantic-ui-react';
+import web3 from '../etheruem/web3';
 
 class RequestRow extends Component {
   render() {
     const { Row, Cell } = Table;
+    const { id, request } = this.props;
     return (
-      
+      <Row>
+        <Cell>{id}</Cell>
+        <Cell>{request.description}</Cell>
+        <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
+        <Cell>{request.recipient}</Cell>
+        <Cell>{request.approvalCount}</Cell>
+        <Cell>{request.approvalCount}</Cell>
+      </Row>
     );
   }
 }
