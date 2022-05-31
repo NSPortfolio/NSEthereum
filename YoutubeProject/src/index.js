c2ed1878aa29184b3b0dc67421606bc90123c63f
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-30 23:52:03 -0500

    Update index.js

--- YoutubeProject/src/index.js
+++ YoutubeProject/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from '/components/search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCZ7eQnrbHY_85XYtE0mrYUi-14cDP8fAQ';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>;
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
