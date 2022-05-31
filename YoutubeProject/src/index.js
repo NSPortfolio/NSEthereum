8e761e89b85677e66ea2e775dc30d01e14942335
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-30 23:49:55 -0500

    Update index.js

--- YoutubeProject/src/index.js
+++ YoutubeProject/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from 'search_bar';
+import SearchBar from '/components/search_bar';
 
 const API_KEY = 'AIzaSyCZ7eQnrbHY_85XYtE0mrYUi-14cDP8fAQ';
 
