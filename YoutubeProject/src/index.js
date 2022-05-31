7b6a32acc983e60d47f699ccf7c8ab34c2259510
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-30 23:28:25 -0500

    Update index.js

--- YoutubeProject/src/index.js
+++ YoutubeProject/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCZ7eQnrbHY_85XYtE0mrYUi-14cDP8fAQ';
+
 const App = () => {
-  return <div>Hi!</div>
+  return <div>Hi!</div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
