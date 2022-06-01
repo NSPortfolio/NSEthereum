c98d5a1fe68147f0ba404766f2d4f44da2db1bea
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-31 21:54:25 -0500

    Update search_bar.js

--- YoutubeProject/src/components/search_bar.js
+++ YoutubeProject/src/components/search_bar.js
@@ -11,10 +11,11 @@ class SearchBar extends Component {
    <div className="search-bar">
     <input 
      value={this.state.term}
-     onChange = {event => this.onInputChange(event.target.value)} />;
+     onChange = {event => this.onInputChange(event.target.value)} />
    </div>
   );
  }
+ 
  onInputChange(term) {
   this.setState({term});
   this.props.onSearchTermChange(term);
