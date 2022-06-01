9cfb78d9f0cf3a3f9c4d06adc6c89b62b24f9231
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-31 20:38:46 -0500

    Update index.js

--- YoutubeProject/src/index.js
+++ YoutubeProject/src/index.js
@@ -9,9 +9,16 @@ const API_KEY = 'AIzaSyCZ7eQnrbHY_85XYtE0mrYUi-14cDP8fAQ';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) {
-      this.setState({ videos });
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+    
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   
@@ -19,7 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
