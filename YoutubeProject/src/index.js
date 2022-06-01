173e0bb66fe193f1de8e4d0c8ba1f029f99e67e5
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-05-31 21:32:20 -0500

    Update index.js

--- YoutubeProject/src/index.js
+++ YoutubeProject/src/index.js
@@ -14,6 +14,10 @@ class App extends Component {
       selectedVideo: null
     };
     
+    this.videoSearch('surfboards');
+  }
+  
+  videoSearch(term) {
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({
         videos: videos,
@@ -25,7 +29,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
