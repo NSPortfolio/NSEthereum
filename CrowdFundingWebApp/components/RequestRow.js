e9bfbaaa56e4c33f0cd22ed1fca5e24afd45d06c
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2023-07-27 19:15:03 -0500

    Update RequestRow.js

--- CrowdFundingWebApp/components/RequestRow.js
+++ CrowdFundingWebApp/components/RequestRow.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'semantic-ui-react';
-import web3 from '../etheruem/web3';
+import web3 from '../ethereum/web3';
 import Campaign from '../ethereum/campaign';
 
 class RequestRow extends Component {
   onApprove = async () => {
     const campaign = Campaign(this.props.address);
     const accounts = await web3.eth.getAccounts();
-    await campaign.methods.approveRequest(this.props.id);
+    await campaign.methods.approveRequest(this.props.id)
       .send({
         from: accounts[0]
       });
@@ -16,7 +16,7 @@ class RequestRow extends Component {
   onFinalize = async () => {
     const campaign = Campaign(this.props.address);
     const accounts = await web3.eth.getAccounts();
-    await campaign.methods.finalizeRequest(this.props.id);
+    await campaign.methods.finalizeRequest(this.props.id)
       .send({
         from: accounts[0]
       });
