f2d263518b8b291c7b893064707e082fe48dcae2
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2023-07-27 19:48:38 -0500

    Update new.js

--- CrowdFundingWebApp/pages/campaigns/requests/new.js
+++ CrowdFundingWebApp/pages/campaigns/requests/new.js
@@ -38,7 +38,7 @@ class RequestNew extends Component {
   render() {
     return (
       <Layout>
-        <Link route={`/campaigns/${this.props.address}/requests`>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
           <a>
             Back
           </a>
