0102bcb49f9017ca927082f0e0baa34b5c1eec48
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2023-07-27 20:12:08 -0500

    Update server.js

--- CrowdFundingWebApp/server.js
+++ CrowdFundingWebApp/server.js
@@ -8,7 +8,7 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, (err)) => {
+  createServer(handler).listen(3000, (err) => {
     if (err) throw err;
   });
 });
